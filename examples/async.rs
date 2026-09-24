5e3ff8047ae1de65915d71df08048307b9d95c8c
Bauke Westendorp <baukewestendorp@gmail.com>
2026-09-24 16:03:08 +0200

    Add downloading to examples

--- examples/async.rs
+++ examples/async.rs
@@ -10,9 +10,15 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         let mut list = client.get_list()?;
         list.sort_by(|a, b| a.fixture.cmp(&b.fixture));
 
-        for entry in list {
+        for entry in &list {
             eprintln!("{}", entry.fixture);
         }
+
+        let file_bytes = client.download_async(list[0].rid).await.unwrap();
+        let download_path = std::path::Path::new(env!("CARGO_MANIFEST_DIR"))
+            .join("examples")
+            .join("downloaded-from-example.gdtf");
+        std::fs::write(download_path, file_bytes).unwrap();
 
         Ok(())
     })
--- examples/blocking.rs
+++ examples/blocking.rs
@@ -9,9 +9,15 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut list = client.get_list()?;
     list.sort_by(|a, b| a.fixture.cmp(&b.fixture));
 
-    for entry in list {
+    for entry in &list {
         eprintln!("{}", entry.fixture);
     }
+
+    let file_bytes = client.download(list[0].rid).unwrap();
+    let download_path = std::path::Path::new(env!("CARGO_MANIFEST_DIR"))
+        .join("examples")
+        .join("downloaded-from-example.gdtf");
+    std::fs::write(download_path, file_bytes).unwrap();
 
     Ok(())
 }
